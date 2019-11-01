272f03453a7bcd3ed4959aa2d7cc094398da293d
Semyon <semyonsinchenko@yahoo.com>
2019-11-01 18:32:30 +0300

    Update nn.py

--- src/nqs_optimizer/nn.py
+++ src/nqs_optimizer/nn.py
@@ -108,7 +108,7 @@ def estimate_all_real_energies(samples, edge_list):
 def estimate_stochastic_reconfiguration_matrix(derivs, l1):
     e_of_prod = tf.einsum("ij,jk -> ik", tf.linalg.adjoint(derivs), derivs)
     avg_deriv = tf.reduce_mean(derivs, axis=0, keepdims=True)
-    prod_of_e = tf.matmul(tf.linalg.adjoin(avg_deriv), avg_deriv)
+    prod_of_e = tf.matmul(tf.linalg.adjoint(avg_deriv), avg_deriv)
     
     SS = e_of_prod - prod_of_e
     reg_part = tf.eye(SS.shape[0], SS.shape[0]) * l1
