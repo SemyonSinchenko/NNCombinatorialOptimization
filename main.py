cc5d9ba6d885810756ad38bd79837e27f10e144f
SemyonSinchenko <semyonsinchenko@yahoo.com>
2019-11-03 00:52:44 +0300

    fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ GRAPH_100_VERTICES = os.path.join("resources", "g05_100.0")
 if __name__ == "__main__":
     edge_list = read_unweighted_edge_list(GRAPH_100_VERTICES)
     problem_dim = get_num_nodes(edge_list)
-    optimizer = tf.keras.optimizer.SGD(1.0e-2, 0.9, nesterov=True, decay=0.999)
+    optimizer = tf.keras.optimizers.SGD(1.0e-2, 0.9, nesterov=True, decay=0.999)
 
     nn = NNMaxCutOptimizer(edge_list, problem_dim, [25, 25, 25, 5], "logdir", epochs=100)
     nn.fit()
