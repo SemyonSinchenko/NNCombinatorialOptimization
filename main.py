5fb92c238b0b06acf9dc9e00b4855237e86fd4ba
SemyonSinchenko <semyonsinchenko@yahoo.com>
2019-11-04 17:25:11 +0300

    params, pinv

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     problem_dim = get_num_nodes(edge_list)
     optimizer = tf.keras.optimizers.SGD(1.0e-2, 0.9, nesterov=True)
 
-    nn = NNMaxCutOptimizer(edge_list, problem_dim, [25, 30, 30, 5], "logdir", optimizer, epochs=500)
+    nn = NNMaxCutOptimizer(edge_list, problem_dim, [15, 50, 50, 5], "logdir", optimizer, epochs=1000)
     nn.fit()
