a7c14dcc304435ef58d5a585268a0bcf704b6d94
Semyon <semyonsinchenko@yahoo.com>
2019-11-06 17:48:17 +0300

    Update linalg_opp.py

--- src/nqs_optimizer/linalg_opp.py
+++ src/nqs_optimizer/linalg_opp.py
@@ -2,7 +2,16 @@
 @author Semyon Sinchenko
 """
 
+from functools import partial
 import tensorflow as tf
+
+
+@tf.function
+def __divide_on_by(x, threshold):
+    if x >= threshold:
+        return tf.constant(1.0) / x
+    else:
+        return tf.constant(0.0)
 
 
 @tf.function
@@ -22,6 +31,6 @@ def moore_penrose_invert(tensor, threshold=tf.constant(1.0e-5)):
     s, u, v = tf.linalg.svd(tensor)
     
     threshold = tf.math.reduce_max(s) * threshold
-    s_inv = tf.linalg.diag(tf.where(s >= threshold, tf.constant(1.0) / s, tf.zeros_like(s)))
+    s_inv = tf.linalg.diag(tf.vectorized_map(partial(__divide_on_by, threshold=threshold), s))
 
     return tf.matmul(v, tf.matmul(s_inv, tf.transpose(u)))
