358429a65eea6e5e9a571df445a434e16833c24e
Semyon <semyonsinchenko@yahoo.com>
2020-01-20 15:39:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ GRAPH_500_VERTICES = os.path.join("resources", "er500")
 GRAPH_1000_VERTICES = os.path.join("resources", "er1000")
 
 if __name__ == "__main__":
-    edge_list = read_unweighted_edge_list(GRAPH_100_VERTICES)
+    edge_list = read_unweighted_edge_list(GRAPH_60_VERTICES)
     problem_dim = get_num_nodes(edge_list)
     optimizer = tf.keras.optimizers.SGD(5.0e-3)
 
     nn = NNMaxCutOptimizer(
         edge_list, problem_dim, [20, 50, 5],
-        "logdir", optimizer, epochs=800, max_samples=5000, drop_first=100, reg_lambda=100
+        "logdir", optimizer, epochs=300, max_samples=1000, drop_first=100, reg_lambda=100
     )
     nn.fit()
