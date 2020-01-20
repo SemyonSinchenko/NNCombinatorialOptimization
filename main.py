f8bf553ac4e8bc78f3158b83872e072dfe422c8c
Semyon <semyonsinchenko@yahoo.com>
2020-01-20 11:50:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ GRAPH_1000_VERTICES = os.path.join("resources", "er1000")
 if __name__ == "__main__":
     edge_list = read_unweighted_edge_list(GRAPH_100_VERTICES)
     problem_dim = get_num_nodes(edge_list)
-    optimizer = tf.keras.optimizers.SGD(1.0e-4, 0.95, nesterov=True)
+    optimizer = tf.keras.optimizers.SGD(1.0e-3, 0.95, nesterov=True)
 
     nn = NNMaxCutOptimizer(
         edge_list, problem_dim, [20, 100, 50, 5],
